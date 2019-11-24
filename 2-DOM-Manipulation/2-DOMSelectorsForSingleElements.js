59bba8da959fb9cab8507e3d4652576cd737797f
Guilherme Girardi <girardi.gui@icloud.com>
2019-11-24 11:20:39 +0000

    second commit

---
+++ 2-DOM-Manipulation/2-DOMSelectorsForSingleElements.js
@@ -0,0 +1,19 @@
+// document.getElementById()
+console.log(document.getElementById('task-title'));
+
+// Get things from the element
+console.log(document.getElementById('task-title').id);
+console.log(document.getElementById('task-title').className);
+
+const taskTitle = document.getElementById('task-title');
+
+// Change styling
+taskTitle.style.background = 'pink';
+taskTitle.style.color = 'red';
+taskTitle.style.padding = '1.5rem';
+//taskTitle.style.display = 'none';
+
+// Change content
+taskTitle.textContent = 'Task List';
+taskTitle.innerText = 'My Tasks';
+taskTitle.innerHTML = '<span style="color:green">Task List</span>';
