51ec52f6807c6c43bbbf7fd215fcfb0c8268d275
Guilherme Girardi <girardi.gui@icloud.com>
2019-11-28 09:02:55 +0000

    prototypes, objectcreate, ES6

---
+++ 8-PrototypeInheritance/app4.js
@@ -0,0 +1,44 @@
+// Person constructor
+// Constructor: A constructor is a function that initializes an object.
+function Person(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+}
+
+// Greeting
+Person.prototype.greeting = function () {
+  return `Hello, ${this.firstName} ${this.lastName}!`;
+}
+
+const person1 = new Person('John', 'Doe');
+
+console.log(person1.greeting());
+
+
+
+
+
+// Customer constructor
+function Customer(firstName, lastName, phone, membership) {
+  Person.call(this, firstName, lastName);
+  this.phone = phone;
+  this.membership = membership;
+}
+
+// Inherit the Person prototype methods
+Customer.prototype = Object.create(Person.prototype);
+
+// Make customer.prototype return Customer()
+Customer.prototype.constructor = Customer;
+
+// Create customer
+const customer1 = new Customer('Tom', 'Smith', '555-555-5555', 'Standard');
+
+console.log(customer1);
+
+// Customer greeting
+Customer.prototype.greeting = function () {
+  return `Hello, ${this.firstName} ${this.lastName}, welcome to our company!`;
+}
+
+console.log(customer1.greeting());
